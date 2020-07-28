652496d2e85999e6f6d86a281e793a42746ee21c
AbibF <gonzalo.moreno@televisatim.com>
2020-07-27 20:43:07 -0500

    FIX: import {  }

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import Lodash from 'lodash';
+import { join }   from 'lodash';
 
 export const suma = (numero1, numero2) => {
   return numero1 + numero2;
 };
 
 export const unirPalabras = (palabra1, palabra2) => {
-  return Lodash.join([palabra1, palabra2], ' ');
+  return join([palabra1, palabra2], ' ');
 };
